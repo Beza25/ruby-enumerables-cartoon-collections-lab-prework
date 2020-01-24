c0e35fee9dc4bac36f2d7ff86444d82194aaf1db
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-24 16:00:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves (dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each do |planeteer_call|
+  new_array =planeteer_calls.map do |planeteer_call|
     puts "#{planeteer_call}.captli"
 
 
