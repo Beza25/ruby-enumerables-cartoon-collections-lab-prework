4c6773c9390e00da0d480cf6964cb3b10e4c01b2
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-24 17:30:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,7 @@ def summon_captain_planet(planeteer_calls)
   new_array
 end
 
-def long_planeteer_calls(short_words, assorted_words)
-  array.select do |
+
   
 def find_the_cheese(array)
   array.find{|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
